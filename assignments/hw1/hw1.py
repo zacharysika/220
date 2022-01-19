b41c1b640206e1727780d846d2e8a9d1702e0fc0
sikaz <sikazc@g.cofc.edu>
2022-01-19 11:10:50 -0500

    homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,13 +1,12 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Zachary Sika
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: It calculates several different prices, areas, volumes, and kilometers quickly and effectively
+through user input.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
@@ -18,19 +17,36 @@ def calc_rec_area():
     print("Area =", area)
 
 def calc_volume():
-
+    length = int(input('What is the length?:'))
+    width = int(input('What is the width?:'))
+    height = int(input('What is the height?:'))
+    volume = length * width * height
+    print("The volume is", volume)
 
 def shooting_percentage():
-    pass
+    shots = int(input('How many shots did you take?:'))
+    success = int(input('How many shots did you hit the target with?:'))
+    percent = success / shots
+    print ('The total percent of shots made is', percent)
+
 
 
 def coffee():
-    pass
+    cost = 0.86
+    coffee = int(input('How much coffee would you like?'))
+    total = coffee * cost
+    print ("Your total for the coffee is: " + total)
 
 
 def kilometers_to_miles():
-    pass
+    conversion = 1.61
+    kilometers = int(input('How many kilometers did you travel?'))
+    miles = kilometers / conversion
+    print ("The total number of miles is:", miles)
+
+
 
 
 if __name__ == '__main__':
     pass
+
