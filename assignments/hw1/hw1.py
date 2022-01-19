c04291cdec9d3683e1a1b3620de10211b11c105a
sikaz <sikazc@g.cofc.edu>
2022-01-19 10:41:50 -0500

    homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -12,11 +12,12 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 def calc_volume():
-    pass
 
 
 def shooting_percentage():
