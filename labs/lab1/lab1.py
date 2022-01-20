d908623cf5a604f33f705a61c6f3ccc9dc4d65e2
sikaz <sikazc@g.cofc.edu>
2022-01-20 14:45:24 -0500

    lab 1

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -4,7 +4,7 @@ lab1.py
 To help a user calculate the monthly interest rates of their account
 I have created this on my own
 """
-def monthly_intrest():
+def monthly_interest():
 annual_interest = eval (input("What is the annual interest rate?"))
 days = eval (input("What is the number of days in the billing cycle?"))
 pbalance = eval(input("What was the previous balance?"))
