d0c9f17bb0b70f67c7da49620a6b151a9b91cd45
sikaz <sikazc@g.cofc.edu>
2022-01-20 14:47:20 -0500

    lab 1

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -5,19 +5,19 @@ To help a user calculate the monthly interest rates of their account
 I have created this on my own
 """
 def monthly_interest():
-annual_interest = eval (input("What is the annual interest rate?"))
-days = eval (input("What is the number of days in the billing cycle?"))
-pbalance = eval(input("What was the previous balance?"))
-payment = eval (input("What is the payment amount?"))
-dpayment = eval (input("What day was the payment made?"))
+    annual_interest = eval (input("What is the annual interest rate?"))
+    days = eval (input("What is the number of days in the billing cycle?"))
+    pbalance = eval(input("What was the previous balance?"))
+    payment = eval (input("What is the payment amount?"))
+    dpayment = eval (input("What day was the payment made?"))
 
-interest = annual_interest / 12
-percentage_interest = interest / 100
+    interest = annual_interest / 12
+    percentage_interest = interest / 100
 
-step1 = pbalance * days
-step2 = payment * (days - dpayment)
-step3 = step1 - step2
-daily_balance = step3 / days
-monthly_interest = daily_balance * percentage_interest
+    step1 = pbalance * days
+    step2 = payment * (days - dpayment)
+    step3 = step1 - step2
+    daily_balance = step3 / days
+    monthly_interest = daily_balance * percentage_interest
 
-print ("Your monthly interest is $", monthly_interest)
+    print ("Your monthly interest is $", monthly_interest)
