2df79dcd3a6d3898ebeb28818fafd439997db1d7
zacharysika <97561965+zacharysika@users.noreply.github.com>
2022-02-18 18:29:13 -0500

    Add files via upload
    difficulty pushing

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -2,7 +2,7 @@
 Name: Zachary Sika
 hw5.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: moves, splits, and separates items and string within a list created by user inputs
 
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
@@ -11,34 +11,47 @@ I certify that this assignment is entirely my own work.
 
 def name_reverse():
     name_entry = input("enter a name(first last):")
-    name = name_entry.split(" ")
-    print(name[1],name[0])
-name_reverse()
+    name = name_entry.split()
+    print(name[1]+",",name[0])
 def company_name():
-    domain = []
+    website_entry = input("enter a domain:")
+    website_name = website_entry.split(".")
+    print(website_name[1])
+
 def initials():
-    abbreviation = []
     roll_over = 0
     student_count = eval(input("how many students are in the class?"))
     for i in range(student_count):
         roll_over = roll_over + 1
-        student = input("What is the name of student")
-        student[abbreviation]
+        student = input("What is the name of student"+" "+str(roll_over)+"?").split()
+        print(student[0][:1]+student[1][:1])
+
 
 def names():
-    pass
-
-
+        pass
 def thirds():
-    pass
-
+    count_over = 0
+    number_sentences = eval(input("Enter the number of sentences"))
+    for i in range(number_sentences):
+        count_over = count_over + 1
+        sentence = input("Enter sentence"+" " + str(count_over)+":")
+        thirds = sentence[0::3]
+    print(thirds)
 
 def word_average():
-    pass
-
+    sum_count = 0
+    sentence = input("enter a sentence:")
+    for i in sentence.split():
+        average = len(i)
+        sum_count = average + sum_count
+    average = (sum_count) / (len(sentence.split()))
+    print (average)
 
 def pig_latin():
-    pass
+    sentence_entry = str(input("enter a sentence to convert to pig latin")).split()
+    for i in sentence_entry:
+        print(i[1:]+ i[0]+ "ay",end = " ")
+    print()
 
 
 if __name__ == '__main__':
