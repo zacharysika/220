607c4195e0fc125970fc6e9a1e1d7725cb641423
zacharysika <97561965+zacharysika@users.noreply.github.com>
2022-03-03 22:51:10 -0500

    Add files via upload

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -8,31 +8,31 @@ file after finding the average grade of the given student's grades and grade wei
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
+import math
 def weighted_average():
-    grade_list = []
+    tally = 1
     infile = open("grades.txt",'w')
     outfile = open("avg.txt",'w')
     student_count = input("How many students are there?:")
     for i in range(int(student_count)):
-        student_names = input("What is the students name?:")
-        print(student_names + ":", file =infile)
-        weights = int(input("What is the weight?:"))
-        grades = int(input("What is the grade?:"))
-        grade_list = grades.split()
+        student_names = input("What is the student's name?:")
+        print(student_names+":", file =infile)
+        weights = input("What is the weight?:")
         weights_list = weights.split()
+        for a in weights_list:
+            weights_list = weights_list * tally
+        grades = input("What is the grade?:")
+        grades_list = grades.split()
+        for b in grades_list:
+            grades_list = grades_list * tally
         print(weights,grades, file = infile)
-        if (weights>100):
-            print("Error: The weights are more than 100.",file = outfile)
-        elif(weights<100):
-            print("Error: The weights are less than 100.",file = outfile)
-        else:
-            print()
+        summary = int(a)*int(b)/100
+        print(student_names+":",summary, file=outfile)
     infile = open("grades.txt","r")
     outfile = open("avg.txt","r")
-    file_read = infile.read(1)
-
-    print(file_read)
-    print(grade_list)
+    file_read = infile.read()
+    infile.close()
+    outfile.close()
 
 
 
